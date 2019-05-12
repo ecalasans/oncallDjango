4634446c8dcfc44abea387211b6000ba9c7f5ca6
Eric <ericcalasans@gmail.com>
2019-05-12 07:17:12 -0300

    Parei no tutorial 2

--- oncall/settings.py
+++ oncall/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'oncall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'oncall_django',
+        'USER': 'eric',
+        'PASSWORD': 'eric123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
