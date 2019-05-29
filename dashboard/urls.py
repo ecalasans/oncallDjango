b15f5fd6f595e8a1789285a7ebcf368fb2e9b742
Eric <ericcalasans@gmail.com>
2019-05-29 18:16:06 -0300

    Terminei a parte 3

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:numero>/', views.details, name='details'),
