87037def799a8b53abcc5715da3afbab7242617f
Eric <ericcalasans@gmail.com>
2019-06-29 22:02:03 -0300

    Login ajeitado em força bruta - campo hidden com value "/dashboard"

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LoginView(LoginRequiredMixin, TemplateView):
-    login_url = '/accounts/login/'
+    login_url = 'accounts/login/'
     template_name = 'registration/login.html'
-
+    extra_context = {
+        'next': '/dashboard',
+    }
 
 class MainView(TemplateView):
     template_name = 'dashboard/index.html'
--- oncall/settings.py
+++ oncall/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'oncall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'dashboard/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,4 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = "/dashboard/"
+LOGIN_REDIRECT_URL = '/dashboard/'
