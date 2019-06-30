f5aba1589b06f6b626157e8c95b12e83ba2021ad
Eric <ericcalasans@gmail.com>
2019-06-30 17:10:38 -0300

    Dump do banco

---
+++ oncall_django_public_django_content_type.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (1, 'dashboard', 'leito');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (2, 'dashboard', 'medico');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (3, 'dashboard', 'paciente');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (4, 'dashboard', 'setor');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (5, 'dashboard', 'paciente_leito');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (6, 'dashboard', 'ocorrencia');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (7, 'admin', 'logentry');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (8, 'auth', 'permission');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (9, 'auth', 'group');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (10, 'auth', 'user');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (11, 'contenttypes', 'contenttype');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (12, 'sessions', 'session');
+INSERT INTO public.django_content_type (id, app_label, model) VALUES (13, 'dashboard', 'hospital');
