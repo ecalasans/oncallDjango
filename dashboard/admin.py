ac7506591b531f2602d7be3410bb04645e079e07
Eric <ericcalasans@gmail.com>
2019-07-01 11:54:53 -0300

    Remodelando banco de dados

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Leito, Setor, Hospital
+from .models import Leito, Setor, Hospital, Medico
 
 
 admin.site.register(Leito)
 admin.site.register(Setor)
 admin.site.register(Hospital)
+admin.site.register(Medico)
 # Register your models here.
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Hospital(models.Model):
@@ -28,7 +29,10 @@ class Leito(models.Model):
 
 class Medico(models.Model):
     crm = models.CharField(default="", max_length=6)
-    email = models.EmailField(default="", max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.user_id.first_name
 
 class Paciente(models.Model):
     nome = models.CharField(max_length=250, default="")
