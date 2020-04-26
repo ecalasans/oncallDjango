a5847c0b267dcd2dcf8e9b6057ded466e1da0d7e
ecalasans <e1906d0103>
2020-04-26 07:02:02 -0300

    #5 - Validação de campos iniciada

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,60 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from .models import Medico
+import re
 
 class MedicoForm(ModelForm):
     class Meta:
         model = Medico
         fields = ['username', 'password', 'first_name', 'last_name', 'email', 'hospital', 'crm']
+
+    def clean_username(self):
+        if len(self.cleaned_data['username']) == 0:
+            raise ValidationError('Este campo é requerido!')
+        else:
+            return self.cleaned_data['username']
+
+    def clean_first_name(self):
+        if len(self.cleaned_data['first_name']) == 0:
+            raise ValidationError('Este campo é requerido!')
+        else:
+            return self.cleaned_data['first_name']
+
+    def clean_last_name(self):
+        if len(self.cleaned_data['last_name']) == 0:
+            raise ValidationError('Este campo é requerido!')
+        else:
+            return self.cleaned_data['last_name']
+
+    def clean_email(self):
+        if len(self.cleaned_data['username']) == 0:
+            raise ValidationError('Este campo é requerido!')
+        else:
+            return self.cleaned_data['username']
+
+    def clean_crm(self):
+        if len(self.cleaned_data['crm']) == 0:
+            raise ValidationError('Este campo é requerido!')
+        else:
+            regex_crm = re.compile('[0-9]+')
+            if (regex_crm.match(self.cleaned_data['crm'])) is not None:
+                return self.cleaned_data['username']
+            else:
+                raise ValidationError('Só pode conter números!!')
+
+    def clean_hospital(self):
+        if self.cleaned_data['hospital'] == 0:
+            raise ValidationError('Selecione um hospital!')
+        else:
+            return self.cleaned_data['hospital']
+
+    def clean_password(self):
+        regex_pass = re.compile('[a-zA-Z0-9!@#$%*_]+')
+
+        if len(self.cleaned_data['password']) > 8:
+            #Verifica o padrão da senha
+            if regex_pass.match(self.cleaned_data['password']) is not None:
+                return self.cleaned_data['password']
+            else:
+                raise ValidationError('Senha deve conter apenas alfanuméricos ou \n !@#$%*_')
+        else:
+            raise ValidationError('Senha deve ter pelo menos 8 caracteres!')
